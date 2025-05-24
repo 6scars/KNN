7afd7c23e269d1062c079ccfce15e5ea1c864cd2
6scars <zasalatol@o2.pl>
2025-05-24 12:43:33 +0200

    Add files via upload
    24.05.2025 z lekcji coś

---
+++ knn/knn/Program.cs
@@ -0,0 +1,35 @@
+﻿public class iris
+{
+    double x;
+    double y;
+    double z;
+    int klasa;
+
+    public iris()
+    {
+        Console.WriteLine("xd");
+    }
+
+
+
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        string sciezka = "D:/170259/knn/dane.txt";
+        if (File.Exists(sciezka))
+        {
+            string[] zawartosc = File.ReadAllLines(sciezka);
+            string pierwszaLinia = zawartosc[0];
+
+            Console.WriteLine(pierwszaLinia);
+        }
+        iris kwiat = new iris();
+
+
+
+    }
+}
