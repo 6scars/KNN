33dc64000b11773ea95f85ca8a8f1cb706c00155
6scars <zasalatol@o2.pl>
2025-05-25 15:30:57 +0200

    Update Program.cs
    dodana funkcja manhatan,
    euklidesowa,
    poprawiona klasa iris
    czytanie pliku txt i zamisanie to w zmiennej typu lista<iris>

--- knn/knn/Program.cs
+++ knn/knn/Program.cs
@@ -1,33 +1,96 @@
-﻿public class iris
+using System.Globalization;
+public class iris
 {
-    double x;
-    double y;
-    double z;
-    int klasa;
+    public int iloscPara = 3;
 
-    public iris()
+    public double x;
+    public double y;
+    public double z;
+    public int klasa = 0;
+
+    public iris(double X, double Y, double Z, int NrClassy =0)
     {
-        Console.WriteLine("xd");
+        x = X;
+        y = Y;
+        z = Z;
+        klasa = NrClassy;
+
     }
 
-
-
+    
 }
 
 
 class Program
 {
+    delegate double Metryka(iris A, iris B);
+
+    static List<iris> stworzListe(string sciezka)
+    {
+        List<iris> stworz = new List<iris>();
+
+        if (File.Exists(sciezka))
+        {
+
+            string[] zawartosc = File.ReadAllLines(sciezka);
+            for (int i = 0; i < zawartosc.Length; i++)
+            {
+                string pierwszaLinia = zawartosc[i];
+                double x = double.Parse(pierwszaLinia.Split('\t')[0], CultureInfo.InvariantCulture);
+                double y = double.Parse(pierwszaLinia.Split('\t')[1], CultureInfo.InvariantCulture);
+                double z = double.Parse(pierwszaLinia.Split('\t')[2], CultureInfo.InvariantCulture);
+                int NrClassy = (int)double.Parse(pierwszaLinia.Split('\t')[3], CultureInfo.InvariantCulture);
+                stworz.Add(new iris(x, y, z, NrClassy));
+            }
+        }
+
+        return stworz; 
+    }
+    
+
+    public static double Euklidesowa(iris A, iris B)
+    {
+        double wynik = 0;
+        double[] Ob1 = {A.x, A.y ,A.z};
+        double[] Ob2 = {B.x, B.y, B.z};
+
+
+        for (int i = 0; i < Ob1.Length; i++)
+        {
+            wynik += Math.Pow(Ob1[i] - Ob2[i], 2);
+        }
+
+        return Math.Sqrt(wynik);
+    }
+    double Manhatan(double[] A, double[]B)
+    {
+        double wynik = 0;
+        for(int i=0; i<A.Length-1; i++)
+        {
+            wynik += Math.Abs(A[i] - B[i]);
+        }
+        return wynik;
+    }
+
+
+
+
     static void Main()
     {
         string sciezka = "D:/170259/knn/dane.txt";
-        if (File.Exists(sciezka))
-        {
-            string[] zawartosc = File.ReadAllLines(sciezka);
-            string pierwszaLinia = zawartosc[0];
+        List<iris> Kwiaty  = stworzListe(sciezka);
 
-            Console.WriteLine(pierwszaLinia);
-        }
-        iris kwiat = new iris();
+
+        Metryka m = Euklidesowa;
+
+
+
+
+        var wynik1 = Euklidesowa(Kwiaty[0], Kwiaty[1]);
+        Console.WriteLine(wynik1);
+        /*var wynik2 = m();*/
+
+
 
 
 
